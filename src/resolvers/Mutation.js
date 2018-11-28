73e2703e30afa9b03fdcfbce35ec3bfdedabe5c1
Marcin Swornowski <swor71@gmail.com>
2018-11-28 18:33:45 +0100

    Add schema for updateItem and deleteItem as well as query item

--- src/resolvers/Mutation.js
+++ src/resolvers/Mutation.js
@@ -1,11 +1,38 @@
 const Mutations = {
-  async createItem(parent, args, ctx, info){
-    const item = await ctx.db.mutation.createItem({
-      data: {
-        ...args
-      }
-    }, info);
+  async createItem(parent, args, ctx, info) {
+    const item = await ctx.db.mutation.createItem(
+      {
+        data: {
+          ...args
+        }
+      },
+      info
+    );
     return item;
+  },
+
+  updateItem(parent, args, ctx, info) {
+    const updates = { ...args };
+    delete updates.id;
+    return ctx.db.mutation.updateItem(
+      {
+        data: updates,
+        where: {
+          id: args.id
+        }
+      },
+      info
+    );
+  },
+
+  async deleteItem(parent, args, ctx, info) {
+    const where = { id: args.id };
+    // 1. find the item
+    const item = await ctx.db.query.item({ where }, `{id, title}`);
+    // 2. check if one owns the item, or has permission
+    // TODO
+    // 3. delete item
+    return ctx.db.mutation.deleteItem({ where }, info);
   }
 };
 
